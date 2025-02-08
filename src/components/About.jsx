26823f6fb4d72ae45ab5990d9f39e98d331c3e77
AayushhGoyal <agayush088@gmail.com>
2025-02-08 21:39:28 +0530

    images ratio set

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,18 @@ import profile from "../assets/githubprofile.jpg";
 
 export default function About() {
   return (
-    <div>
-      <img src={profile} alt="Profile" className="h-52 w-52 rounded-full" />
+    <div className="relative">
+      <img
+        src={profile}
+        alt="Progile"
+        className="absolute w-[100%] h-[400px] object-cover"
+      />
+
+      <img
+        src={profile}
+        alt="Profile"
+        className=" absolute h-60 w-60 border-white border-4 rounded-full z-10 mt-[300px] ml-9"
+      />
     </div>
   );
 }
