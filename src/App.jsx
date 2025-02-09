aafcafcc4d20b899f94a04480cbed4656869f707
AayushhGoyal <agayush088@gmail.com>
2025-02-09 23:20:44 +0530

    added my information

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Information from "./components/Information";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="h-auto w-full bg-gradient-radial from-slate-900 to-slate-950 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] flex justify-center items-center">
         <div className="bg-white h-[200vh] w-[890px]">
           <About />
+          <Information />
         </div>
       </div>
     </div>
---
+++ src/components/Information.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Information() {
+  return (
+    <div className="flex">
+      <div className="z-10 mt-[450px] ml-80">
+        <div className="text-4xl ml-4">Ayush Goyal </div>
+        <div className="text-lg ml-4 mt-3">Full Stack Developer </div>
+        <div className="text-lg ml-4">open source enthusiast </div>
+      </div>
+    </div>
+  );
+}
