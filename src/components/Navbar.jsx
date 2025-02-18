2db0714fd7da0e45145b6fa154c3d3b259757b46
AayushhGoyal <agayush088@gmail.com>
2025-02-18 14:11:44 +0530

    navbar content text changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,17 +20,19 @@ const navBarItems = [
 export default function Navbar() {
   return (
     <div>
-      <div className="flex justify-center gap-x-20 h-16 items-center bg-black text-white">
-        {navBarItems.map((item, index) => {
-          return (
-            <div
-              className="hover:scale-110 hover:text-gray-400 transition duration-300"
-              key={index}
-            >
-              <div>{item.name}</div>
-            </div>
-          );
-        })}
+      <div className="flex justify-center gap-x-20 h-16 items-center bg-black">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-inherit-purple-400 via-green-300 to-orange-400 flex gap-x-24">
+          {navBarItems.map((item, index) => {
+            return (
+              <div
+                className="hover:scale-110 hover:text-white transition duration-300"
+                key={index}
+              >
+                <div className=" ">{item.name}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
